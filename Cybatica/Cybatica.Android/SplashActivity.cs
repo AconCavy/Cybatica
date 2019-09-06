c9a3a36e53eee35aa3c7c7a790c28fe2b3947b81
AconCavy <AconCavy@gmail.com>
2019-09-06 16:43:29 +0900

    Add launch screen

---
+++ Cybatica/Cybatica.Android/SplashActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Content;
+using Xamarin.Forms.Platform.Android;
+
+namespace Cybatica.Droid
+{
+    [Activity(Theme = "@style/MainTheme.Splash", Icon = "@mipmap/icon_cybatica", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : FormsAppCompatActivity
+    {
+        protected override void OnResume()
+        {
+            base.OnResume();
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
