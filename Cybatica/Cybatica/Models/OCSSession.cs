6f08f674679a472bb8764ff1a8b5662939c744f7
AconCavy <AconCavy@gmail.com>
2019-12-03 23:21:32 +0900

    Update Ocs session

--- Cybatica/Cybatica/Models/OCSSession.cs
+++ Cybatica/Cybatica/Models/OCSSession.cs
@@ -1,8 +1,9 @@
-﻿using DynamicData;
+﻿using System;
+using DynamicData;
 
 namespace Cybatica.Models
 {
-    public struct AnalysisData
+    public readonly struct AnalysisData : IEquatable<AnalysisData>
     {
         public float Value { get; }
         public double Timestamp { get; }
@@ -17,43 +18,96 @@ namespace Cybatica.Models
         {
             return $"{Value},{Timestamp}";
         }
+
+        public bool Equals(AnalysisData other)
+        {
+            return Value.Equals(other.Value) && Timestamp.Equals(other.Timestamp);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is AnalysisData other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Value.GetHashCode() * 397) ^ Timestamp.GetHashCode();
+            }
+        }
     }
 
     public class OcsSession
     {
+        private readonly SourceList<AnalysisData> _meanEda;
+        private readonly SourceList<AnalysisData> _nnMean;
+        private readonly SourceList<AnalysisData> _ocs;
+        private readonly SourceList<AnalysisData> _peakEda;
+        private readonly SourceList<AnalysisData> _sdNn;
+
         public OcsSession()
         {
-            Ocs = new SourceList<AnalysisData>();
-            NnMean = new SourceList<AnalysisData>();
-            SdNn = new SourceList<AnalysisData>();
-            MeanEda = new SourceList<AnalysisData>();
-            PeakEda = new SourceList<AnalysisData>();
+            _ocs = new SourceList<AnalysisData>();
+            _nnMean = new SourceList<AnalysisData>();
+            _sdNn = new SourceList<AnalysisData>();
+            _meanEda = new SourceList<AnalysisData>();
+            _peakEda = new SourceList<AnalysisData>();
 
-            InitializeSession();
+            Ocs = _ocs.AsObservableList();
+            NnMean = _nnMean.AsObservableList();
+            SdNn = _sdNn.AsObservableList();
+            MeanEda = _meanEda.AsObservableList();
+            PeakEda = _peakEda.AsObservableList();
         }
 
-        public SourceList<AnalysisData> Ocs { get; }
-        public SourceList<AnalysisData> NnMean { get; }
-        public SourceList<AnalysisData> SdNn { get; }
-        public SourceList<AnalysisData> MeanEda { get; }
-        public SourceList<AnalysisData> PeakEda { get; }
+        public IObservableList<AnalysisData> Ocs { get; }
+        public IObservableList<AnalysisData> NnMean { get; }
+        public IObservableList<AnalysisData> SdNn { get; }
+        public IObservableList<AnalysisData> MeanEda { get; }
+        public IObservableList<AnalysisData> PeakEda { get; }
 
         public void InitializeSession()
         {
-            Ocs.Clear();
-            Ocs.Add(new AnalysisData(0, 0));
+            _ocs.Clear();
+            _ocs.Add(new AnalysisData(0, 0));
 
-            NnMean.Clear();
-            NnMean.Add(new AnalysisData(0, 0));
+            _nnMean.Clear();
+            _nnMean.Add(new AnalysisData(0, 0));
 
-            SdNn.Clear();
-            SdNn.Add(new AnalysisData(0, 0));
+            _sdNn.Clear();
+            _sdNn.Add(new AnalysisData(0, 0));
 
-            MeanEda.Clear();
-            MeanEda.Add(new AnalysisData(0, 0));
+            _meanEda.Clear();
+            _meanEda.Add(new AnalysisData(0, 0));
 
-            PeakEda.Clear();
-            PeakEda.Add(new AnalysisData(0, 0));
+            _peakEda.Clear();
+            _peakEda.Add(new AnalysisData(0, 0));
+        }
+
+        public void AddOcs(in AnalysisData ocs)
+        {
+            _ocs.Add(ocs);
+        }
+
+        public void AddNnMean(in AnalysisData nnMean)
+        {
+            _nnMean.Add(nnMean);
+        }
+
+        public void AddSdNn(in AnalysisData sdNn)
+        {
+            _sdNn.Add(sdNn);
+        }
+
+        public void AddMeanEda(in AnalysisData meanEda)
+        {
+            _meanEda.Add(meanEda);
+        }
+
+        public void AddPeakEda(in AnalysisData peakEda)
+        {
+            _peakEda.Add(peakEda);
         }
     }
 }
