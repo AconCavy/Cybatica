eb92d55d8feff8d793302535a44fb2a14dfc04d3
AconCavy <AconCavy@gmail.com>
2019-12-03 23:22:12 +0900

    Add hot observable connector of the Empatica and Ocs sessions

---
+++ Cybatica/Cybatica/Empatica/IEmpaticaSessionConnector.cs
@@ -0,0 +1,24 @@
+﻿using DynamicData;
+using System;
+
+namespace Cybatica.Empatica
+{
+    public interface IEmpaticaSessionConnector
+    {
+        IObservable<IChangeSet<Acceleration>> AccelerationConnectable { get; }
+
+        IObservable<IChangeSet<BatteryLevel>> BatteryConnectable { get; }
+        
+        IObservable<IChangeSet<Bvp>> BvpConnectable { get; }
+
+        IObservable<IChangeSet<Gsr>> GsrConnectable { get; }
+
+        IObservable<IChangeSet<Hr>> HrConnectable { get; }
+
+        IObservable<IChangeSet<Ibi>> IbiConnectable { get; }
+
+        IObservable<IChangeSet<Tag>> TagConnectable { get; }
+
+        IObservable<IChangeSet<Temperature>> TemperatureConnectable { get; }
+    }
+}
---
+++ Cybatica/Cybatica/Services/IOcsSessionConnector.cs
@@ -0,0 +1,19 @@
+﻿using Cybatica.Models;
+using DynamicData;
+using System;
+
+namespace Cybatica.Services
+{
+    public interface IOcsSessionConnector
+    {
+        IObservable<IChangeSet<AnalysisData>> OcsConnectable { get; }
+
+        IObservable<IChangeSet<AnalysisData>> NnMeanConnectable { get; }
+
+        IObservable<IChangeSet<AnalysisData>> SdNnConnectable { get; }
+
+        IObservable<IChangeSet<AnalysisData>> MeanEdaConnectable { get; }
+
+        IObservable<IChangeSet<AnalysisData>> PeakEdaConnectable { get; }
+    }
+}
