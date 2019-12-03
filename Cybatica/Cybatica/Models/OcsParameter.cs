a198f764e950d17abc798e45c0ff547da47f41d1
AconCavy <AconCavy@gmail.com>
2019-12-03 23:22:49 +0900

    Add DTO which coefficient numbers of Ocs from json

---
+++ Cybatica/Cybatica/Models/OcsParameter.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Cybatica.Models
+{
+    public class OcsParameter
+    {
+        [JsonPropertyName("Parameter")] public string Name { get; set; }
+        public float NnMean { get; set; }
+        public float SdNn { get; set; }
+        public float MeanEda { get; set; }
+        public float PeakEda { get; set; }
+        public float Intercept { get; set; }
+    }
+}
