2753223ec5afeb7d5261bf4947c63a447abec1cc
AconCavy <AconCavy@gmail.com>
2019-12-03 23:22:35 +0900

    Change model of parameters on view to interface

---
+++ Cybatica/Cybatica/Empatica/IEmpaticaModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Cybatica.Empatica
+{
+    public interface IEmpaticaModel
+    {
+        Acceleration Acceleration { get; }
+        BatteryLevel BatteryLevel { get; }
+        Bvp Bvp { get; }
+        Gsr Gsr { get; }
+        Hr Hr { get; }
+        Ibi Ibi { get; }
+        Tag Tag { get; }
+        Temperature Temperature { get; }
+    }
+}
---
+++ Cybatica/Cybatica/Services/IOcsModel.cs
@@ -0,0 +1,13 @@
+﻿using Cybatica.Models;
+
+namespace Cybatica.Services
+{
+    public interface IOcsModel
+    {
+        AnalysisData Ocs { get; }
+        AnalysisData NnMean { get; }
+        AnalysisData SdNn { get; }
+        AnalysisData MeanEda { get; }
+        AnalysisData PeakEda { get; }
+    }
+}
