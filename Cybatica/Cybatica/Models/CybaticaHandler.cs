a3a2ef3e71bab98ecf7be671f19aaeec95a76f95
AconCavy <AconCavy@gmail.com>
2020-01-09 22:27:14 +0900

    Resolve conflict

--- Cybatica/Cybatica/Models/CybaticaHandler.cs
+++ Cybatica/Cybatica/Models/CybaticaHandler.cs
@@ -113,12 +113,6 @@ namespace Cybatica.Models
                 .Where(_ => _isDataSession)
                 .Subscribe(_ =>
                 {
-<<<<<<< HEAD
-                    var nnMeanRatio = _ocsSession.NnMean.Items.LastOrDefault().Value / _baseNnMeanAve;
-                    var sdNnRatio = _ocsSession.SdNn.Items.LastOrDefault().Value / _baseSdNnAve;
-                    var meanEdaRatio = _ocsSession.MeanEda.Items.LastOrDefault().Value / _baseMeanEdaAve;
-                    var peakEdaRatio = _ocsSession.PeakEda.Items.LastOrDefault().Value / _basePeakEdaAve;
-=======
                     var tmp = _ocsSession.NnMean.Items.LastOrDefault().Value;
                     var nnMeanRatio = Math.Abs(tmp) > 0.01f ? tmp / _baseNnMeanAve : 1f;
 
@@ -131,7 +125,6 @@ namespace Cybatica.Models
                     tmp = _ocsSession.PeakEda.Items.LastOrDefault().Value;
                     var peakEdaRatio = Math.Abs(tmp) > 0.01f ? tmp / _basePeakEdaAve : 1f;
 
->>>>>>> Modify OCS
                     var calculatedOcs =
                         new AnalysisData(calculator.CalculateOcs(nnMeanRatio, sdNnRatio, meanEdaRatio, peakEdaRatio),
                             DateTimeOffset.Now.ToUnixTimeSeconds() - _startedTime);
